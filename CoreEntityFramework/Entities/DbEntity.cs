0f41ffbb19cb75c758a612da17b23289e296ace8
Ben McCallum <ben.mccallum@live.com.au>
2016-07-25 12:10:45 +1000

    Force Date fields to be datetime2 type to avoid the following exception on use.
    "The conversion of a datetime2 data type to a datetime data type resulted in an out-of-range value.
    The statement has been terminated."

--- CoreEntityFramework/Entities/DbEntity.cs
+++ CoreEntityFramework/Entities/DbEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CoreEntityFramework.Entities
 {
@@ -8,9 +9,11 @@ namespace CoreEntityFramework.Entities
     /// </summary>
     public abstract class DbEntity
     {
+        [Column(TypeName = "datetime2")]
         [DataType(DataType.DateTime)]
         public DateTime DateCreatedUtc { get; set; }
 
+        [Column(TypeName = "datetime2")]
         [DataType(DataType.DateTime)]
         public DateTime DateLastModifiedUtc { get; set; }
     }
